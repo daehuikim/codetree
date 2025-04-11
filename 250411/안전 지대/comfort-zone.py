1d139098a4ad135780447245225bfaa22fc54760
andreaKim <40377750+daehuikim@users.noreply.github.com>
2025-04-11 15:07:06 +0900

    Title: 안전 지대, Time: 85ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250411/안전 지대/comfort-zone.py
@@ -0,0 +1,40 @@
+import copy
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+max_value = max(max(row) for row in grid)
+# Please write your code here.
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+
+def can_go(x, y, k, visited):
+    if 0 <= x < m and 0 <= y < n and grid[y][x] > k and not visited[y][x]:
+        return True
+    return False
+
+
+def dfs(x, y, depth, visited):
+    visited[y][x] = True
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if can_go(nx, ny, depth, visited):
+            dfs(nx, ny, depth, visited)
+
+max_area = 0
+for t in range(1, max_value):
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    areas = 0
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j] and grid[i][j] > t:
+                dfs(j,i,t,visited)
+                areas += 1
+    if max_area < areas:
+        max_area = areas
+    else:
+        t-=1
+        break
+
+print(t, max_area)
+
