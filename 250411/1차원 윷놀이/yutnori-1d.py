41a4a85cd86992b4cb7c6dbf16aa1330cd72291d
andreaKim <40377750+daehuikim@users.noreply.github.com>
2025-04-11 11:29:28 +0900

    Title: 1차원 윷놀이, Time: 2747ms, Memory: 25MB, Status: Passed - Codetree

--- 250411/1차원 윷놀이/yutnori-1d.py
+++ 250411/1차원 윷놀이/yutnori-1d.py
@@ -1,41 +1,28 @@
 n, m, k = map(int, input().split())
 nums = list(map(int, input().split()))
-
+pieces = [1 for _ in range(k)]
+ans=0
 # Please write your code here.
-def goal_check(answer,m):
-    count=0
-    count_dict = {item[0]: 0 for item in answer}
-    for a in answer:
-        count_dict[a[0]]+=a[1]
-    for val in count_dict.values():
-        if val >= m-1:
-            count += 1
-    return count
+def calc():
+    score = 0
+    for piece in pieces:
+        score += (piece >= m)
+    return score
 
-def choose(depth,current,k,n,m):
-    if depth==n:
-        return goal_check(current,m)
-    
-    count_dict = {i: 0 for i in range(k)}
-    for a in current:
-        count_dict[a[0]] += a[1]
+def find_max(cnt):
+    global ans
+    ans = max(ans, calc())
 
-    remaining = sum(nums[depth:])  # 남은 명령 점수 총합
-    possible = 0
+    if cnt == n: 
+        return
+
     for i in range(k):
-        if count_dict[i] + remaining >= m:  # m 도달 가능성 있는 agent 수
-            possible += 1
+        if pieces[i] >= m:
+            continue
+        
+        pieces[i] += nums[cnt]
+        find_max(cnt + 1)
+        pieces[i] -= nums[cnt]
 
-    # pruning: 지금 가능한 최대값이 너무 작으면 의미 없음
-    if possible <= 0:
-        return 0
-
-    count=0
-    num = nums[depth]
-    for i in range(k):
-        current.append((i,num))
-        count = max(count, choose(depth+1,current,k,n,m))
-        current.pop()
-    return count
-
-print(choose(0,[],k,n,m))
+find_max(0)
+print(ans)
