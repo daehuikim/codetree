b11f61d1e1e03fb3f9d9745fb973cd297630ae5f
andreaKim <40377750+daehuikim@users.noreply.github.com>
2025-04-12 11:27:29 +0900

    Title: 사각형 채우기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250412/사각형 채우기 2/rectangle-fill-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+MOD=10007
+# Please write your code here.
+memo=[0 for _ in range(n+10)]
+memo[0]=1
+memo[1]=1
+memo[2]=3
+for i in range(2,n+1):
+    memo[i]=(memo[i-1]+2*memo[i-2])%MOD
+
+print(memo[n])
